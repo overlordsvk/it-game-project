caa2f7ab8c789611273b7fef0301f3d59f6001dc
Peter Baltazarovič <pepokuk@gmail.com>
2018-11-03 16:49:02 +0100

    Dto

---
+++ pv179/BL/QueryObject/GroupQueryObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BL.DTO;
+using BL.DTO.Filters;
+using Game.DAL.Entity.Entities;
+using Game.Infrastructure.Query;
+using Game.Infrastructure.Query.Predicates;
+using Game.Infrastructure.Query.Predicates.Operators;
+
+namespace BL.QueryObject
+{
+    public class GroupQueryObject : QueryObjectBase<GroupDto, Group, GroupFilterDto, IQuery<Group>>
+    {
+        public GroupQueryObject(IMapper mapper, IQuery<Group> query) : base(mapper, query)
+        {
+        }
+
+        protected override IQuery<Group> ApplyWhereClause(IQuery<Group> query, GroupFilterDto filter)
+        {
+
+            return string.IsNullOrWhiteSpace(filter.Name) 
+                ? query 
+                : query.Where(new SimplePredicate(nameof(Group.Name),
+                                                ValueComparingOperator.StringContains,
+                                                filter.Name));
+        }
+    }
+}
--- pv179/pv179-console/Program.cs
+++ pv179/pv179-console/Program.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Linq;
+﻿using Game.DAL.Entity.Entities;
+using System;
 using System.Threading.Tasks;
-using AutoMapper;
-using BL.DTO;
-using BL.DTO.Common;
-using BL.DTO.Filters;
-using BL.QueryObject;
-using Castle.Windsor;
-using Castle.Windsor.Installer;
-using Game.DAL.Entity.Entities;
-using Game.Infrastructure;
-using Game.Infrastructure.Query;
-using Game.Infrastructure.UnitOfWork;
+using Game.Infrastructure.Entity.UnitOfWork;
+using Game.Infrastructure.Entity.Repository;
 
 namespace PV179Console
 {
@@ -21,95 +12,87 @@ namespace PV179Console
         {
 
             PrintDbContent().Wait();
+            Console.WriteLine("Test");
 
             Console.ReadKey();
+            /*using (var dbContext = new GameDbContext())
+            {
+                dbContext.Database.Delete();
+            }*/
         }
         public static async Task PrintDbContent()
         {
-            using (var container = new WindsorContainer())
+            /*
+            var provider = EntityUnitOfWorkProvider.Create();
+            using (var unitOfWork = provider.Create())
             {
-                container.Install(FromAssembly.This());
+                var accountRepo = new EntityRepository<Account>(provider);
+                var fightRepo = new EntityRepository<Fight>(provider);
+                var groupRepo = new EntityRepository<Group>(provider);
+                var characterRepo = new EntityRepository<Character>(provider);
+                var grouppostRepo = new EntityRepository<GroupPost>(provider);
+                var itemRepo = new EntityRepository<Item>(provider);
+                var messageRepo = new EntityRepository<Message>(provider);
+                var wtypeRepo = new EntityRepository<WeaponType>(provider);
 
-                var provider = container.Resolve<IUnitOfWorkProvider>();
+                var accounts = await accountRepo.GetAllAsync();
+                var fights = await fightRepo.GetAllAsync();
+                var groups = await groupRepo.GetAllAsync();
+                var characters = await characterRepo.GetAllAsync();
+                var groupposts = await grouppostRepo.GetAllAsync();
+                var items = await itemRepo.GetAllAsync();
+                var messages = await messageRepo.GetAllAsync();
+                var wtypes = await wtypeRepo.GetAllAsync();
 
-                //var provider = EntityUnitOfWorkProvider.Create();
-                using (var unitOfWork = provider.Create())
+                Console.WriteLine("\nAccounts: ");
+                foreach (var acc in accounts)
                 {
-                    var queryObjAccount = new AccountQueryObject(container.Resolve<IMapper>(), container.Resolve<IQuery<Account>>());
-                    QueryResultDto<AccountDto,AccountFilterDto> res = queryObjAccount.ExecuteQuery(new AccountFilterDto {Email = "navi@ivan.com"}).Result;
-                    
-                    Console.WriteLine("#####"+res.Items.First().Username);
-                    var accountRepo = container.Resolve<IRepository<Account>>(provider);
-                    var fightRepo = container.Resolve<IRepository<Fight>>(provider);
-                    var groupRepo = container.Resolve<IRepository<Group>>(provider);
-                    var characterRepo = container.Resolve<IRepository<Character>>(provider);
-                    var testgroupRepo = container.Resolve<IRepository<GroupPost>>(provider);
-                    var itemRepo = container.Resolve<IRepository<Item>>(provider);
-                    var messageRepo = container.Resolve<IRepository<Message>>(provider);
-                    var typeRepo = container.Resolve<IRepository<WeaponType>>(provider);
-
-                    var accounts = await accountRepo.GetAllAsync();
-                    var fights = await fightRepo.GetAllAsync();
-                    var groups = await groupRepo.GetAllAsync();
-                    var characters = await characterRepo.GetAllAsync();
-                    var testgroup = await testgroupRepo.GetAllAsync();
-                    var items = await itemRepo.GetAllAsync();
-                    var messages = await messageRepo.GetAllAsync();
-                    var types = await typeRepo.GetAllAsync();
-
-                    Console.WriteLine("\nAccounts: ");
-                    foreach (var acc in accounts)
-                    {
-                        Console.WriteLine($"{acc.Id} \t  {acc.Username}  \t  {acc.Email}  \t \t Character:   {acc.Character?.Name}");
-                    }
-
-                    
-
-                    Console.WriteLine("\nCharacters: ");
-                    foreach (var ch in characters)
-                    {
-                        Console.WriteLine($"{ch.Id}  \t  {ch.Name} \t Items: {ch.Items.Count} \t Shop: {ch.Shop.Count} \t {ch.Group.Name} \t RM: {ch.ReceivedMessages.Count} \t SM: {ch.SentMessages.Count} \t Owner:  {ch.Account.Username}");
-                    }
-
-                    Console.WriteLine("\nWeapon Types: ");
-                    foreach (var wt in types)
-                    {
-                        Console.WriteLine($"{wt.Id} \t {wt.ItemName} \t MA: {wt.MaxAttack:5} MD: {wt.MinDefense:5}");
-                    }
-
-                    Console.WriteLine("\nItems: ");
-                    foreach (var i in items)
-                    {
-                        //Console.WriteLine($"{i.Id} \t {i.Name}  \t  {i.WeaponType.ItemName}   \t  \t   Owner: {i.Owner?.Name}");
-                        Console.WriteLine("{0,-5}{1,-20}{2,-20}{3,-20}", i.Id, i.Name, i.WeaponType.ItemName, "Owner: " + i.Owner?.Name);
-                    }
-
-                    Console.WriteLine("\nMessages: ");
-                    foreach (var m in messages)
-                    {
-                        Console.WriteLine("{0,-5}{1,-20}{2,-20}{3,-20}{4,-20}", m.Id, m.Sender.Name, m.Receiver.Name, "Sub: " + m.Subject, "Text: " + m.Text);
-                    }
-
-                    Console.WriteLine("\nGroups: ");
-                    foreach (var g in groups)
-                    {
-                        Console.WriteLine($"{g.Id} \t {g.Name}  Members: {g.Members.Count}  Wall: {g.Wall.Count} ");
-                    }
-
-                    Console.WriteLine("\nGroupPosts: ");
-                    foreach (var g in testgroup)
-                    {
-                        Console.WriteLine($"{g.Id} \t {g.Group.Name} \t {g.Author.Name} : {g.Text} ");
-                    }
-
-                    Console.WriteLine("\nFights: ");
-                    foreach (var f in fights)
-                    {
-                        Console.WriteLine($"{f.Id} \t {f.Attacker.Name} \t {f.Defender.Name} \t Ai: {f.AttackerItem.Name} \t Di: {f.DefenderItem.Name} \t Succ: {f.AttackSuccess}");
-                    }
+                    Console.WriteLine($"{acc.Id} \t  {acc.Username}  \t  {acc.Email}  \t \t Character:   {acc.Character?.Name}");
                 }
-            }
-        
+
+                Console.WriteLine("\nCharacters: ");
+                foreach (var ch in characters)
+                {
+                    Console.WriteLine($"{ch.Id}  \t  {ch.Name} \t Items: {ch.Items.Count} \t Shop: {ch.Shop.Count} \t {ch.Group.Name} \t RM: {ch.ReceivedMessages.Count} \t SM: {ch.SentMessages.Count} \t Owner:  {ch.Account.Username}");
+                }
+
+                Console.WriteLine("\nWeapon Types: ");
+                foreach (var wt in wtypes)
+                {
+                    Console.WriteLine($"{wt.Id} \t {wt.ItemName} \t MA: {wt.MaxAttack:5} MD: {wt.MinDefense:5}");
+                }
+
+                Console.WriteLine("\nItems: ");
+                foreach (var i in items)
+                {
+                    //Console.WriteLine($"{i.Id} \t {i.Name}  \t  {i.WeaponType.ItemName}   \t  \t   Owner: {i.Owner?.Name}");
+                    Console.WriteLine("{0,-5}{1,-20}{2,-20}{3,-20}", i.Id,i.Name,i.WeaponType.ItemName, "Owner: " + i.Owner?.Name);
+                }
+
+                Console.WriteLine("\nMessages: ");
+                foreach (var m in messages)
+                {
+                    Console.WriteLine("{0,-5}{1,-20}{2,-20}{3,-20}{4,-20}", m.Id, m.Sender.Name, m.Receiver.Name, "Sub: " + m.Subject,"Text: " + m.Text);
+                }
+
+                Console.WriteLine("\nGroups: ");
+                foreach (var g in groups)
+                {
+                    Console.WriteLine($"{g.Id} \t {g.Name}  Members: {g.Members.Count}  Wall: {g.Wall.Count} ");
+                }
+
+                Console.WriteLine("\nGroupPosts: ");
+                foreach (var g in groupposts)
+                {
+                    Console.WriteLine($"{g.Id} \t {g.Group.Name} \t {g.Author.Name} : {g.Text} ");
+                }
+
+                Console.WriteLine("\nFights: ");
+                foreach (var f in fights)
+                {
+                    Console.WriteLine($"{f.Id} \t {f.Attacker.Name} \t {f.Defender.Name} \t Ai: {f.AttackerItem.Name} \t Di: {f.DefenderItem.Name} \t Succ: {f.AttackSuccess}");
+                }
+            }*/
         }
     }
 }
