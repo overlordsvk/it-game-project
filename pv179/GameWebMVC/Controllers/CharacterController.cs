1ae14d14e70217b88e01b829078e9d4c70605876
NumBit <baltazarovicp@gmail.com>
2018-11-13 20:21:39 +0100

    task

---
+++ pv179/GameWebMVC/Controllers/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using BL.Facades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameWebMVC.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public CharacterFacade characterFacade { get; set; }
+
+        // GET: Character
+        public async Task<ActionResult> Index(Guid id)
+        {
+            var character = await characterFacade.GetCharacterById(id);
+            return View(character);
+        }
+    }
+}
