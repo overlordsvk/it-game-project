05694074842d4c8ff2d01b5cd94ce798eb59d89f
overlordsvk <molcanym@gmail.com>
2018-12-08 23:06:54 +0100

    admin group

--- pv179/BL/DTO/GroupDto.cs
+++ pv179/BL/DTO/GroupDto.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,17 +12,17 @@ namespace BL.DTO
 {
     public class GroupDto : DtoBase
     {
-        [Required, MinLength(4, ErrorMessage = "Názov skupiny musí mať aspoň 4 znaky"), MaxLength(64, ErrorMessage = "Názov skupiny nesmie mať viac ako 64 znakov")]
+        [Required, MinLength(4, ErrorMessage = "Názov skupiny musí mať aspoň 4 znaky"), MaxLength(64, ErrorMessage = "Názov skupiny nesmie mať viac ako 64 znakov"), DisplayName("Meno skupiny")]
         public string Name { get; set; }
 
-        [StringLength(512, ErrorMessage = "Skúste iný obrázok")]
+        [StringLength(512, ErrorMessage = "Skúste iný obrázok"), DisplayName("Obrázok")]
         public string Picture { get; set; }
 
         public ICollection<CharacterDto> Members { get; set; }
         [JsonIgnore]
         public ICollection<GroupPostDto> Wall { get; set; }
 
-        [StringLength(4096, ErrorMessage = "Popis skupiny môže mať max. 4096 znakov")]
+        [StringLength(4096, ErrorMessage = "Popis skupiny môže mať max. 4096 znakov"), DisplayName("Popis")]
         public string Description { get; set; }
     }
 }
