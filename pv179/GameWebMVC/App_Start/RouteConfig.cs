94dc3f6a710abf721add0a451de17f30a394b87a
overlordsvk <molcanym@gmail.com>
2019-01-12 17:55:20 +0100

    add route do Admin area

--- pv179/GameWebMVC/App_Start/RouteConfig.cs
+++ pv179/GameWebMVC/App_Start/RouteConfig.cs
@@ -14,12 +14,14 @@ namespace GameWebMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Admin",
+                url: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            //routes.MapMvcAttributeRoutes();
         }
     }
 }
